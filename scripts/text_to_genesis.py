d205b28e7396e48ec026974ba8d747c4f94a66c0
hess_ja <jan-timo.hesse@dlr.de>
2021-12-07 16:30:25 +0100

    updated Scripts

--- scripts/text_to_genesis.py
+++ scripts/text_to_genesis.py
@@ -1,22 +1,72 @@
 #!/usr/bin/env python
 
+""" text_to_genesis.py:  Converts a meshfree discretization from the Peridigm text file format to the genesis/exodus file format." """
+
+__author__ = "David Littlewood (djlittl@sandia.gov)"
+
+# ************************************************************************
+#
+#
+#                             Peridigm
+#                 Copyright (2011) Sandia Corporation
+#
+# Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+# the U.S. Government retains certain rights in this software.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met
+#
+# 1. Redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the Corporation nor the names of the
+# contributors may be used to endorse or promote products derived from
+# this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY SANDIA CORPORATION "AS IS" AND ANY
+# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL SANDIA CORPORATION OR THE
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# Questions?
+# David J. Littlewood   djlittl@sandia.gov
+# John A. Mitchell      jamitch@sandia.gov
+# Michael L. Parks      mlparks@sandia.gov
+# Stewart A. Silling    sasilli@sandia.gov
+#
+# ************************************************************************
+
 import sys
-import string
 import os
 
 # The following points to the location of exodus.py
-path_to_exodus_py = '/usr/local/src/seacas/lib/'
+path_to_exodus_py = 'trilinos_install_path/lib'
 
 # The path above must also point to the netcdf libraries.  These libraries are not generally included in
 # the Trilinos install.  One solution is to create symbolic links in this directory that point to the
 # netcdf libraries (which you built as a TPL for Trilinos)
 
 sys.path.append(path_to_exodus_py)
-import exodus2 as exodus
+
+if sys.version_info >= (3, 0):
+  import exodus3 as exodus
+else:
+  import exodus2 as exodus
 
 def read_line(file):
     """Scans the input file and ignores lines starting with a '#' or '\n'."""
-    
     buff = file.readline()
     if len(buff) == 0: return None
     while buff[0] == '#' or buff[0] == '\n':
@@ -24,29 +74,7 @@ def read_line(file):
         if len(buff) == 0: return None
     return buff
 
-if __name__ == "__main__":
-
-    print "\n---- Text to Genesis\n"
-
-    if len(sys.argv) < 2:
-        print "Usage:  text_to_genesis.py <discretization_file.txt> <nodeset_1.txt> <nodeset_2.txt> ... <nodeset_n.txt>\n"
-        print "The discretization file lists the nodes as (x, y, z, block_id, volume)"
-        print "The node set files list the node numbers in each node set (1-based indexing)\n"
-        sys.exit(1)
-
-    textFileName = sys.argv[1]
-    nodeSetFileNames = []
-    for i in range(len(sys.argv)-2):
-        nodeSetFileNames.append(sys.argv[i+2])
-
-    print "Discretization file:"
-    print " ", textFileName
-    print
-    print "Node set files:"
-    for i in range(len(nodeSetFileNames)):
-        print " ", nodeSetFileNames[i]
-    print
-    
+def read_text_file(textFileName):
     textFile = open(textFileName)
 
     # The text file contains (x, y, z, block_id, vol)
@@ -60,7 +88,7 @@ if __name__ == "__main__":
     buff = read_line(textFile)
     while buff != None:
         nodeId += 1
-        vals = string.splitfields(buff)
+        vals = buff.split()
         X.append(float(vals[0]))
         Y.append(float(vals[1]))
         Z.append(float(vals[2]))
@@ -71,9 +99,12 @@ if __name__ == "__main__":
         blocks[block_id].append(nodeId)
         buff = read_line(textFile)
     textFile.close()
-    
-    print "Read", len(X), "nodes and", len(blocks), "block from", textFileName
 
+    print("Read {} nodes and {} block from {}".format(len(X),len(blocks),textFileName))
+
+    return X, Y, Z, vol, blocks
+
+def read_node_set_files(nodeSetFileNames):
     nodeSets = []
     for fileName in nodeSetFileNames:
         nodeSets.append([])
@@ -81,14 +112,17 @@ if __name__ == "__main__":
         nodeSetFile = open(fileName)
         buff = read_line(nodeSetFile)
         while buff != None:
-            vals = string.splitfields(buff)
+            vals = buff.split()
             for val in vals:
                 node_id = int(val)
                 nodeSets[node_set_id-1].append(node_id)
             buff = read_line(nodeSetFile)
         nodeSetFile.close()
-        print "Read", len(nodeSets[node_set_id-1]), "node ids from", fileName
+        print("Read {} node ids from {}".format(len(nodeSets[node_set_id-1]),fileName))
 
+    return nodeSets
+
+def write_exodus_file(textFileName, X, Y, Z, vol, blocks, nodeSets):
     # Write the Exodus II file
     exodusFileName = textFileName[:-4] + ".g"
     exodusTitle = "Text to Genesis translation of " + textFileName[:-4]
@@ -145,7 +179,7 @@ if __name__ == "__main__":
     # 1) The so-called SPH radius, which is NOT the actual radius of the sphere and is NOT used by Peridigm
     # 2) The element volume
     for blockId in exodusBlockIds:
-    
+
         numberOfPoints = len(blocks[blockId])
         exodusElementConnectivity = blocks[blockId]
 
@@ -164,7 +198,7 @@ if __name__ == "__main__":
 
         # Write element attributes
         exodusFile.put_elem_attr(blockId, exodusElementAttributes)
-        
+
     # Write the node sets
     #   node_set_params (id, numSetNodes, numSetDistFacts)
     #   node_set_nodes
@@ -179,4 +213,43 @@ if __name__ == "__main__":
     # Close the output Exodus file
     exodusFile.close()
 
-    print "\nData written to Exodus II file", exodusFileName, "\n"
+    print("\nData written to Exodus II file {}\n".format(exodusFileName))
+
+
+def main():
+    """
+      Main routine
+    """
+    print("\n---- Text to Genesis\n")
+
+    usage()
+
+    textFileName = sys.argv[1]
+    nodeSetFileNames = []
+    for i in range(len(sys.argv)-2):
+        nodeSetFileNames.append(sys.argv[i+2])
+
+    print("Discretization file:")
+    print(" {}".format(textFileName))
+    print("")
+    print("Node set files:")
+    for i in range(len(nodeSetFileNames)):
+        print(" {}".format(nodeSetFileNames[i]))
+    print("")
+
+    X, Y, Z, vol, blocks = read_text_file(textFileName)
+
+    nodeSets = read_node_set_files(nodeSetFileNames)
+
+    write_exodus_file(textFileName, X, Y, Z, vol, blocks, nodeSets)
+
+def usage():
+    if len(sys.argv) < 2:
+        print("Usage:  text_to_genesis.py <discretization_file.txt> <nodeset_1.txt> <nodeset_2.txt> ... <node(et_n.txt>\n")
+        print("The discretization file lists the nodes as (x, y, z, block_id, volume)")
+        print("The node set files list the node numbers in each node set (1-based indexing)\n")
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
