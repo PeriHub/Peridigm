698a15e6c3c171e89d77fdfeaf08057d541cb023
hess_ja <jan-timo.hesse@dlr.de>
2022-01-12 14:53:27 +0100

    Fixed ElasticPlasticCorrespondenceMaterial

--- src/materials/Peridigm_ElasticPlasticCorrespondenceMaterial.cpp
+++ src/materials/Peridigm_ElasticPlasticCorrespondenceMaterial.cpp
@@ -110,8 +110,10 @@ PeridigmNS::ElasticPlasticCorrespondenceMaterial::ElasticPlasticCorrespondenceMa
   double C11=0.0, C44=0.0, C55=0.0, C66=0.0, C12=0.0, C13=0.0, C14=0.0, C15=0.0, C16=0.0, C22=0.0, C33=0.0;
   double  C23=0.0, C24=0.0, C25=0.0, C26=0.0, C34=0.0, C35=0.0, C36=0.0, C45=0.0, C46=0.0, C56=0.0;
   
+  bool iso = false;
   if (params.isParameter("Material Symmetry")){
     if (params.get<string>("Material Symmetry")=="Isotropic"){
+       iso = true;
        C11 = params.get<double>("C11");
        C44 = params.get<double>("C44");
        C55 = params.get<double>("C44");
@@ -160,30 +162,31 @@ PeridigmNS::ElasticPlasticCorrespondenceMaterial::ElasticPlasticCorrespondenceMa
    }
   }
   else{
-       m_bulkModulus = calculateBulkModulus(params);
-       m_shearModulus = calculateShearModulus(params);
-  
-       C11 = 2*m_shearModulus + m_bulkModulus;
-       C44 = m_shearModulus;
-       C55 = m_shearModulus;
-       C66 = m_shearModulus;
-       C12 = C11 - 2*C55;
-       C13 = C12;
-       C14 = 0.0;
-       C15 = 0.0;
-       C16 = 0.0;
-       C22 = 2*m_shearModulus + m_bulkModulus;
-       C33 = 2*m_shearModulus + m_bulkModulus;
-       C23 = C12;
-       C24 = 0.0;
-       C25 = 0.0;
-       C26 = 0.0;
-       C34 = 0.0;
-       C35 = 0.0;
-       C36 = 0.0;
-       C45 = 0.0;
-       C46 = 0.0;
-       C56 = 0.0;
+    iso = true;
+    m_bulkModulus = calculateBulkModulus(params);
+    m_shearModulus = calculateShearModulus(params);
+    double lam = m_bulkModulus - 2./3.*m_shearModulus; 
+    C11 = 2*m_shearModulus + lam;
+    C44 = m_shearModulus;
+    C55 = m_shearModulus;
+    C66 = m_shearModulus;
+    C12 = C11 - 2.*C55;
+    C13 = C12;
+    C14 = 0.0;
+    C15 = 0.0;
+    C16 = 0.0;
+    C22 = 2.*m_shearModulus + lam;
+    C33 = 2.*m_shearModulus + lam;
+    C23 = C12;
+    C24 = 0.0;
+    C25 = 0.0;
+    C26 = 0.0;
+    C34 = 0.0;
+    C35 = 0.0;
+    C36 = 0.0;
+    C45 = 0.0;
+    C46 = 0.0;
+    C56 = 0.0;
   }
 // Equation (8) Dipasquale, D., Sarego, G., Zaccariotto, M., Galvanetto, U., A discussion on failure criteria
   // for ordinary state-based Peridynamics, Engineering Fracture Mechanics (2017), doi: https://doi.org/10.1016/
@@ -199,7 +202,7 @@ PeridigmNS::ElasticPlasticCorrespondenceMaterial::ElasticPlasticCorrespondenceMa
    C[5][0] = C16;C[5][1] = C26;C[5][2]= C36; C[5][3] = C46; C[5][4] = C56; C[5][5]= C66;
   }
   // tbd in future
-  if (m_planeStress==true){
+  if (m_planeStress==true&&iso == true){
   //    //only transversal isotropic in the moment
    C[0][0] = C11-C13*C13/C22;C[0][1] = C12-C13*C23/C22;C[0][2] = 0.0; C[0][3] = 0.0; C[0][4] = 0.0; C[0][5] = 0.0;
    C[1][0] = C12-C13*C23/C22;C[1][1] = C22-C13*C23/C22;C[1][2] = 0.0; C[1][3] = 0.0; C[1][4] = 0.0; C[1][5] = 0.0;
