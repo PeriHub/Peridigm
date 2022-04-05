774b40e384c0e55e6102ae3e6e3d1dd13ae641db
will_cr <christian.willberg@dlr.de>
2022-04-05 10:13:19 +0200

    2D - 3D elastic fortran

--- src/materials/umats/base.f
+++ src/materials/umats/base.f
@@ -8,7 +8,7 @@ C    INCLUDE 'ABA_PARAM.INC'
       implicit real(8) (a-h,o-z)
 C
       CHARACTER*80 CMNAME
-      DOUBLE PRECISION, DIMENSION(6,6) :: C
+      DOUBLE PRECISION, DIMENSION(NTENS,NTENS) :: C
       DOUBLE PRECISION, DIMENSION(NTENS) :: STRANNP1
       DIMENSION STRESS(NTENS),STATEV(NSTATV),
      1 DDSDDE(NTENS,NTENS),
@@ -18,26 +18,36 @@ C
      5 JSTEP(4)
 C
 C  EVALUATE NEW STRESS TENSOR
-C     
+C    
       DO K1=1,NTENS
-            STRANNP1 = STRAN(K1) + DSTRAN(K1)
-      END DO
-      DO K1=1,NTENS
+            STRANNP1(K1) = STRAN(K1) + DSTRAN(K1)
             DO K2=1,NTENS
                   C(K1,K2) = 0.0
             END DO
       END DO
-      C(1,1) = PROPS(1)-PROPS(2)*PROPS(2)/PROPS(1)
-      C(1,2) = PROPS(2)-PROPS(2)*PROPS(2)/PROPS(1)
-      C(2,1) = PROPS(2)-PROPS(2)*PROPS(2)/PROPS(1)
-      C(2,2) = PROPS(1)-PROPS(2)*PROPS(2)/PROPS(1)
-      C(6,6) = PROPS(3)
-      DO K1 =1,6
-            STRESS(K1) = 0.D0
-            DO K2 = 1, 6
-                  STRESS(K1) = STRESS(K1) + C(K1,K2) *
-     &            (STRAN(K2) + DSTRAN(K2))
-            END DO
-      END DO 
+      IF NTENS == 6 THEN
+            C(1,1) = PROPS(1)-PROPS(2)*PROPS(2)/PROPS(1)
+            C(2,2) = PROPS(1)-PROPS(2)*PROPS(2)/PROPS(1)
+            C(3,3) = PROPS(1)-PROPS(2)*PROPS(2)/PROPS(1)
+            C(1,2) = PROPS(2)-PROPS(2)*PROPS(2)/PROPS(1)
+            C(2,1) = PROPS(2)-PROPS(2)*PROPS(2)/PROPS(1)
+            C(3,1) = PROPS(2)-PROPS(2)*PROPS(2)/PROPS(1)
+            C(1,3) = PROPS(2)-PROPS(2)*PROPS(2)/PROPS(1)
+            C(2,3) = PROPS(2)-PROPS(2)*PROPS(2)/PROPS(1)
+            C(3,2) = PROPS(2)-PROPS(2)*PROPS(2)/PROPS(1)
+            C(4,4) = PROPS(3)
+            C(5,5) = PROPS(3)
+            C(6,6) = PROPS(3)
+ 
+      ELSE
+            C(1,1) = PROPS(1)-PROPS(2)*PROPS(2)/PROPS(1)
+            C(2,2) = PROPS(1)-PROPS(2)*PROPS(2)/PROPS(1)
+            C(1,2) = PROPS(2)-PROPS(2)*PROPS(2)/PROPS(1)
+            C(2,1) = PROPS(2)-PROPS(2)*PROPS(2)/PROPS(1)
+            C(3,3) = PROPS(3)
+
+      END IF      
+
+      STRESS = MATMUL(C, STRANNP1)
       RETURN
       END
