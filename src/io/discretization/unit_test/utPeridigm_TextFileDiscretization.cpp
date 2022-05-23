41915a0a32d8897bcb7b1eb4dcd88b9891a4f542
will_cr <christian.willberg@dlr.de>
2022-05-23 15:44:22 +0200

    adding test file

---
+++ src/io/discretization/unit_test/utPeridigm_TextFileDiscretization.cpp
@@ -0,0 +1,4 @@
+//
+//  double PeridigmNS::TextFileDiscretization::get_max_dist(const vector<double> &coordinates, const double coorAvg[3],
+//                                                             const vector<int> &topo)
+//
